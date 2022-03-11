bc3dd5f87f715f51cce239f5260d9533674f8f5e
Alexander Zubkov <alex@MacBook-Air-Alexander.local>
2022-03-11 20:29:03 +0300

    ex14 framework created

--- ex6_long_redirect.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-url = response.url
-redirects = len(response.history)
-
-print(f"Кол-во редиректов: {redirects}")
-print(f"Итоговый url {url}")
--- test_short_phrase_ex10.py
+++
@@ -1,7 +0,0 @@
-
-class TestPhrase:
-    def test_short_phrase(self):
-        phrase = input("Set a phrase: ")
-        max_len = 15
-
-        assert len(phrase) <= max_len, f"Your phrase has more than {max_len} characters"
