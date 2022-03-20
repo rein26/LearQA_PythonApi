6bfaf326914e4159260828f9b6a605ace74222ac
Alexander Zubkov <alex@MacBook-Air-Alexander.local>
2022-03-20 23:03:50 +0300

    added ex_16: Requesting another users data

--- lib/assertions.py
+++ lib/assertions.py
@@ -41,6 +41,16 @@ class Assertions:
         assert name not in response_as_dict, f"Response JSON shouldn't have not ket '{name}. But it's present"
 
     @staticmethod
+    def assert_json_has_not_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecodeError:
+            assert False, f"Response is not JSON format.Response text is '{response.text}"
+
+        for name in names:
+            assert name not in response_as_dict, f"Response JSON shouldn't have not key '{name}'. But it's present"
+
+    @staticmethod
     def assert_code_status(response: Response, expecter_status_code):
         assert response.status_code == expecter_status_code, \
             f"Unexpected status code! Expected: {expecter_status_code}. Actual: {response.status_code}"
--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -7,9 +7,8 @@ class TestUserGet(BaseCase):
         response = MyRequests.get("/user/2")
 
         Assertions.assert_json_has_key(response, "username")
-        Assertions.assert_json_has_not_key(response, "email")
-        Assertions.assert_json_has_not_key(response, "firstName")
-        Assertions.assert_json_has_not_key(response, "lastName")
+        unexpected_fields = ["email", "firstName", "lastName"]
+        Assertions.assert_json_has_not_keys(response, unexpected_fields)
 
     def test_get_user_details_auth_as_same_user(self):
         data = {
@@ -31,3 +30,25 @@ class TestUserGet(BaseCase):
 
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_details_another_user(self):
+        data = {
+            "email": "vinkotov@example.com",
+            "password": "1234"
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        another_user_id = 28892
+
+        response2 = MyRequests.get(
+            f"/user/{another_user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_json_has_key(response2, "username")
+        unexpected_fields = ["email", "firstName", "lastName"]
+        Assertions.assert_json_has_not_keys(response2, unexpected_fields)
